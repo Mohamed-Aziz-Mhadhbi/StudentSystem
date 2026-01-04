ebeefdc789ce61958862ba2a5366f1cbad720be6
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2026-01-04 14:51:17 +0100

    update

--- src/main/java/com/spring/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/spring/studentsystem/service/StudentServiceImpl.java
@@ -1,5 +1,16 @@
 package com.spring.studentsystem.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.spring.studentsystem.model.Student;
+import com.spring.studentsystem.repository.StudentRepository;
 
-public class StudentServiceImpl {
+public class StudentServiceImpl implements StudentService {
+	
+	@Autowired
+	private StudentRepository studentrepository;
+
+	@Override
+	public Student saveStudent(Student student) {
+		return studentrepository.save(student);
+	}
 
 }
