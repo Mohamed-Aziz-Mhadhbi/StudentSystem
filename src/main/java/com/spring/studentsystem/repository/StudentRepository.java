9d9f9c738b540b4c1e88db6e4cd4c838d7efce5f
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2026-01-04 14:42:06 +0100

    update

--- src/main/java/com/spring/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/spring/studentsystem/repository/StudentRepository.java
@@ -1,5 +1,9 @@
 package com.spring.studentsystem.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.spring.studentsystem.model.Student;
 
-public interface StudentRepository {
+@Repository  
+public interface StudentRepository extends JpaRepository<Student, Integer>{
 
 }
